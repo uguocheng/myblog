016e83401675949281c35267fff1d1be6baa59bf
v_guocwu <v_guocwu@tencent.com>
2021-02-08 20:07:05 +0800

    注册

--- myBlog/enroll-server/src/main/java/com/gcnbl/controller/IndexController.java
+++ myBlog/enroll-server/src/main/java/com/gcnbl/controller/IndexController.java
@@ -3,7 +3,9 @@ package com.gcnbl.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/index")
@@ -12,5 +14,21 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("hello", "gcblog");
         return "index";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam("username") String username,
+                           @RequestParam("password") String password) {
+        return null;
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username,
+                        @RequestParam("password") String password) {
+        //如果没有记录，则进行注册
+        if (false){
+            return "redirct:/index/register";
+        }
+        return null;
     }
 }
