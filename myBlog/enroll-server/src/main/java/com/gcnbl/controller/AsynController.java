5beff98aec091684b9d7a783f1441217097a6ddd
v_guocwu <v_guocwu@tencent.com>
2021-02-26 18:05:21 +0800

    增加前端页面异步验证用户名是否可用；问题thymeleaf如何引用本地jq

--- myBlog/enroll-server/src/main/java/com/gcnbl/controller/AsynController.java
+++ myBlog/enroll-server/src/main/java/com/gcnbl/controller/AsynController.java
@@ -1,6 +1,6 @@
 package com.gcnbl.controller;
 
-import com.alibaba.fastjson.JSONObject;
+import com.gcnbl.results.Result;
 import com.gcnbl.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,24 +8,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.Map;
 
 @RestController
-
 public class AsynController {
 
     @Autowired
     private UserService userService;
 
+    private Result<HashMap> result=Result.OK();
+
     @RequestMapping("/blog/registerName")
-    public Object registerName(@RequestParam("username") String username) {
+    public Result<HashMap> registerName(@RequestParam("username") String username) {
         System.out.println(username);
 
         int i = userService.validateName(username);
 
         System.out.println("i="+i);
 
-        Map<String,Object> map = new HashMap<>();
+        HashMap<String,Object> map = new HashMap<>();
         if (i==0){
             map.put("userExist", false);
             map.put("msg", "该用户名可用");
@@ -34,7 +34,8 @@ public class AsynController {
             map.put("msg", "用户名已存在");
         }
 
-        //将msp转换为json并传递给前端
-        return JSONObject.toJSON(map);
+        result.setModel(map);
+
+        return result;  //JSONObject.toJSON(map) 也可直接将map转换为json并传递给前端
     }
 }
